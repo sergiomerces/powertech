c83a520b1fdaa7e30d2a46dcaa9d8c1d44dd3a70
sergiomerces <sergio.merces@gmail.com>
2023-04-14 02:36:31 -0300

    manipulando listas

---
+++ 06-listas/aula.js
@@ -0,0 +1,28 @@
+//declara uuma lista (array)
+const alunos = ['João', 'Vitor', 'Marina'];
+
+//para acessar uma lista
+console.log(alunos);
+
+//para acessar um elemento da lista usamos o seu índice
+console.log(alunos[0]);
+
+//para adicionar um novo elemento numa posição específica
+alunos[3] = 'Vinicius';
+console.log(alunos);
+
+//para adicionar um elemento ao fim da lista 
+alunos.push('Renan');
+console.log(alunos);
+
+//para remover um elemento do fim da lista
+alunos.pop();
+console.log(alunos);
+
+//para remover um elemento ao início da lista
+alunos.shift('André');
+console.log(alunos);
+
+//para adicionar um elemento ao início da lista
+alunos.unshift('José');
+console.log(alunos);
