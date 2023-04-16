6b9d3618e8fad10c9acdbffde1a3b2faf12de930
sergiomerces <sergio.merces@gmail.com>
2023-04-15 23:56:33 -0300

    importação - exportação e destructuring

---
+++ 07-importacao/main.js
@@ -0,0 +1,11 @@
+//o require é uma função do nodejs quue importa arquivos
+
+//referenciar o que está sendo importado como uma constante
+const funcoes = require('./funcoes-auxiliares');
+
+//imprimir o objeto que foi importado
+console.log(funcoes);
+
+//imprimir a função
+console.log(funcoes.gets());
+
